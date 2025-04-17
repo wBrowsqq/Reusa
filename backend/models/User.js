7f83cb2cf0d70ab18bdaa06bf5137e3b32cd93b4
NickNinjaGit <balguing@gmail.com>
2025-04-17 00:12:00 -0300

    add image field to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,9 @@ const User = db.define("User", {
     type: DataTypes.INTEGER,
     allowNull: false
   },
+  image: {
+    type: DataTypes.STRING,
+  },
   isAdmin: {
     type: DataTypes.BOOLEAN,
     allowNull: false
